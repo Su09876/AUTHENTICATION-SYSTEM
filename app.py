74a9f90b2b16e40b4c38be0749dd35aa2c67f0cc
Subhranshu Panda <109572151+Su09876@users.noreply.github.com>
2026-01-30 23:40:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['MYSQL_PORT']= int(os.getenv('MYSQL_PORT', 3306))
 
 
 mysql = MySQL(app)
